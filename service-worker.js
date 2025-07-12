0447da71ffc6b2a6ab0f6b673f0c1e2effeefb85
Cooper <frankidyfrank3@Gmail.com>
2025-07-12 13:43:36 -0500

    pwa?

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'dfpwm-player-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
+  './',
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png',
   // any other scripts/CSS you need
 ];
 
